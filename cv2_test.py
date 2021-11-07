0ffde6b9f0efcbe4a98a95bd72130545f80f2405
Alexander Rodionov <alx1dr@gmail.com>
2021-11-07 15:54:29 +0300

    [works, 60fps, 1920x1080] add text mvp

--- cv2_test.py
+++ cv2_test.py
@@ -6,8 +6,12 @@ import sink
 
 
 im = np.ones(shape=(config.frame_height, config.frame_width,3), dtype=np.int16)
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 
 with sink.ffmpeg() as f:
     while True:
-        cv2.rectangle(im, pt1=util.random_xy(), pt2=util.random_xy(), color=(255, 255, 0), thickness=1)
+        im[...] = 0
+        cv2.rectangle(im, pt1=util.random_xy(), pt2=util.random_xy(), color=(255, 255, 0), thickness=10)
+        cv2.putText(im, util.random_text(), util.random_xy(), font, fontScale=1, color=(255, 255, 255), thickness=2, lineType=cv2.LINE_AA)
         f.write(im.tobytes())
